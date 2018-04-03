1d8422c92cfd1e7f841baf4ffe0219c9b4c4de0c
willgriff <will.a.griffiths@gmail.com>
2018-04-03 15:11:45 +0100

    js update-columns alteration working

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(function(){
   const fruits =  ['banana', 'grapes', 'pear', 'peach', 'lemon', 'greenApple', 'redApple', 'orange', 'strawberry', 'raspberry', 'plumb', 'watermelon', 'pineapple', 'blueberry',
-'cherry'];
+'cherry',''];
 
   // create a randomly generated 2-Dimensional array with integers----
   function createGround(width, height){
@@ -27,59 +27,21 @@ $(function(){
     });
   }
 
-  function shuffleColumn(column){
-    //loop over column and move each element in column j one down
-    $ground[j].each(function() {
-      $(this).append('hello');
-    });
-  }
+  // console.log( $ground[j]);
 
   function buildCell(cell, i, j){
-    var $elem = $(`<div class="basetile" data-i='${i}' data-j='${j}'/>`);
-    if (cell === 0){
-      $mapWrap.append($elem.addClass('banana'));
-    } if (cell === 1){
-      $mapWrap.append($elem.addClass('grapes'));
-    } if (cell === 2){
-      $mapWrap.append($elem.addClass('pear'));
-    }if (cell === 3){
-      $mapWrap.append($elem.addClass('peach'));
-    } if (cell === 4){
-      $mapWrap.append($elem.addClass('lemon'));
-    } if (cell === 5){
-      $mapWrap.append($elem.addClass('greenApple'));
-    } if (cell === 6){
-      $mapWrap.append($elem.addClass('redApple'));
-    }if (cell === 7){
-      $mapWrap.append($elem.addClass('orange'));
-    } if (cell === 8){
-      $mapWrap.append($elem.addClass('strawberry'));
-    } if (cell === 9){
-      $mapWrap.append($elem.addClass('raspberry'));
-    } if (cell === 10){
-      $mapWrap.append($elem.addClass('plumb'));
-    }if (cell === 11){
-      $mapWrap.append($elem.addClass('watermelon'));
-    } if (cell === 12){
-      $mapWrap.append($elem.addClass('pineapple'));
-    } if (cell === 13){
-      $mapWrap.append($elem.addClass('blueberry'));
-    }if (cell === 14){
-      $mapWrap.append($elem.addClass('cherry'));
-    }
-    var $first = true;
-    var $counter = 0;
+    var $elem = $(`<div class="basetile" id="cell_${i}_${j}" data-i='${i}' data-j='${j}'/>`);
+    $elem.addClass(fruits[cell]);
+    $elem.data('fruit', fruits[cell]);
+    $mapWrap.append($elem);
 
-
-
+    var $counter = 20;
     $elem.click(function(){
-      $counter += 5;
+      $counter += 10;
       if (cell === $fruitSelected) {
-        $first = false;
-        $('.player1').css('width',`${$counter}`+'px');
-        console.log(`<div class="basetile" data-i='${i}' data-j='${j}'/>`);
         $elem.removeClass(fruits[cell]);
-        shuffleColumn(j);
+        // $('.player1').css('width',`${$counter}`+'px');
+        moveColumnDown(parseInt($(this).data('i')), parseInt($(this).data('j')));
       }
       if ($counter >= 180){
         $('.player1').css('width', '180px');
@@ -91,6 +53,16 @@ $(function(){
 
   }
   mapBuilder();
+
+  function moveColumnDown(i,j){
+    for(let x = (i-1); x>= 0; x--){
+      const $cell = $(`#cell_${x}_${j}`);
+      console.log($cell.data('fruit'));
+      const $emptyCell = $(`#cell_${(x+1)}_${j}`);
+      $cell.removeClass($cell.data('fruit'));
+      $emptyCell.addClass($cell.data('fruit'));
+    }
+  }
 
   // Generate a random value from the 2-dimensional array
   function getRandomLine() {
@@ -107,6 +79,8 @@ $(function(){
 
   function displayFruit(){
     $speechBubbleFruit.addClass(fruits[$fruitSelected]);
+    // const $cell = $(`#cell_${x}_${j}`);
+    // console.log($cell.data('fruit'));
   }
 
   displayFruit();
@@ -114,28 +88,28 @@ $(function(){
   $(document).ready(function () {
     setTimeout(function(){
       $('#ready').fadeIn(100);
-    }, 1000);
+    }, 100);
     setTimeout(function(){
       $('#ready').fadeOut(100);
-    }, 2000);
+    }, 200);
     setTimeout(function(){
       $('#set').fadeIn(100);
-    }, 3000);
+    }, 300);
     setTimeout(function(){
       $('#set').fadeOut(100);
-    }, 4000);
+    }, 400);
     setTimeout(function(){
       $('#go').fadeIn(100);
-    }, 5000);
+    }, 500);
     setTimeout(function(){
       $('#go').fadeOut(100);
-    }, 6000);
+    }, 600);
     setTimeout(function(){
       $('.speechBubble').fadeIn(300);
-    }, 4000);
+    }, 400);
     setTimeout(function(){
       $('.fruitSpeech').fadeIn(100);
-    }, 6000);
+    }, 600);
 
 
     setTimeout(function(){
